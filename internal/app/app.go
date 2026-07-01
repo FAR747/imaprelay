dcee89077ddcdfcbaacbbf91e42151f4b57694e6
FAR747 <far@hightgames.ru>
2026-07-02 02:10:37 +0300

    add app.go

---
+++ internal/app/app.go
@@ -0,0 +1,54 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/FAR747/imaprelay/internal/config"
+)
+
+const DefaultCheckTime = 60 // in seconds
+
+func Run(cfg *config.Config) error {
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	fmt.Println("ImapRelay started")
+
+	if err := runOnce(ctx, cfg); err != nil {
+		fmt.Printf("poll error: %v\n", err)
+	}
+
+	checkTime := time.Duration(DefaultCheckTime) * time.Second // TODO: add interval in config
+	ticker := time.NewTicker(checkTime)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("App stopped")
+			return nil
+
+		case <-ticker.C:
+			if err := runOnce(ctx, cfg); err != nil {
+				fmt.Printf("poll error: %v\n", err)
+			}
+		}
+	}
+}
+
+func runOnce(ctx context.Context, cfg *config.Config) error {
+	for _, imap := range cfg.IMAPs {
+		fmt.Printf("Checking IMAP account: %s\n", imap.Name)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/FAR747/imaprelay/internal/app"
 	"github.com/FAR747/imaprelay/internal/config"
 	"os"
 )
@@ -33,4 +34,9 @@ func main() {
 	fmt.Printf("Starting ImapRelay %s...\n", version)
 	fmt.Printf("config: %s\n", *configPath)
 	fmt.Printf("imaps: %d\n", len(cfg.IMAPs))
+
+	if err := app.Run(cfg); err != nil {
+		fmt.Fprintf(os.Stderr, "app error: %v\n", err)
+		os.Exit(1)
+	}
 }
